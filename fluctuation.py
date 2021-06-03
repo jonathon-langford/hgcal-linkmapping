e57c4a0446910da52bad5925a725c8c8ef8a4e7d
Samuel Webb <samuel.webb@cern.ch>
2021-06-03 17:00:46 +0100

    change parameter name from includePhi60 to includePhiDivisionY

--- fluctuation.py
+++ fluctuation.py
@@ -568,11 +568,11 @@ def main():
     
     if (config['function']['plot_MeanMax']):
         subconfig = config['plot_MeanMax']
-        plotMeanMax(eventData = subconfig['eventData'], outdir = config['output_dir'], includePhi60 = subconfig['includePhi60'], binningConfig = binningConfig )
+        plotMeanMax(eventData = subconfig['eventData'], outdir = config['output_dir'], includePhiDivisionY = subconfig['includePhiDivisionY'], binningConfig = binningConfig )
 
     if (config['function']['plot_Truncation']):
         subconfig = config['plot_Truncation']
-        plotTruncation(eventData = subconfig['eventData'],outdir = config['output_dir'], includePhi60 = subconfig['includePhi60'], binningConfig = binningConfig )
+        plotTruncation(eventData = subconfig['eventData'],outdir = config['output_dir'], includePhiDivisionY = subconfig['includePhiDivisionY'], binningConfig = binningConfig )
         
     if (config['function']['studyTruncationOptions']):
         subconfig = config['studyTruncationOptions']
